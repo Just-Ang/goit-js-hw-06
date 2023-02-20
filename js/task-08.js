ec71bb4815a20cfc8d85b80113cdcc8efba04364
Angelica <lisoveckaya1997@gmail.com>
2023-02-20 20:10:51 +0200

    changed 8 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,29 @@
+const formEl = document.querySelector('.login-form');
+console.log(formEl);
+
+
+const handleSubmit = (event)  => {
+    event.preventDefault();
+
+    const {
+        elements: { email, password }
+      } = event.currentTarget;
+
+      if (email.value === "" || password.value === "") {
+        window.alert('Всі поля повинні бути заповнені!');
+      }
+
+      const data = {
+        Login: `${email.value}`,
+        Password: `${password.value}`,
+      }
+      console.log(data);
+
+      
+      event.currentTarget.reset();
+
+}
+
+formEl.addEventListener("submit", handleSubmit);
+
 
