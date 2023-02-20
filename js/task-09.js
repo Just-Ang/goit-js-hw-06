cfa9d9681327637c73990969cf4857795b81bd73
Angelica <lisoveckaya1997@gmail.com>
2023-02-20 20:40:25 +0200

    changed 9 task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,27 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector('body');
+console.log(bodyEl)
+
+const buttonEl = document.querySelector('.change-color');
+console.log(buttonEl)
+
+
+const colorEl = document.querySelector('.color')
+
+
+
+
+const changeColor = () => {
+  console.log('changed')
+
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  console.log(bodyEl.style.backgroundColor)
+
+  colorEl.textContent = bodyEl.style.backgroundColor;
+
+}
+
+buttonEl.addEventListener('click', changeColor);
