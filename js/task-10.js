3b9a90d5bbbecd06e868bdf4a0bba39f2d47914a
Angelica <lisoveckaya1997@gmail.com>
2023-02-21 20:57:05 +0200

    10 task

--- js/task-10.js
+++ js/task-10.js
@@ -2,4 +2,34 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+const controlsEL = document.querySelector('input');
+const createBtnEl = document.querySelector('[data-create]');
+const destroyBtnEl = document.querySelector('[data-destroy]');
+const boxesEl = document.querySelector('#boxes');
 
+
+const createBoxes = (amount) => {
+  let size = 30;
+
+  amount = controlsEL.value;
+ 
+  for (let i = 0; i < amount; i += 1) {
+    const div = document.createElement('div');
+   // console.log(div);
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+
+    size += 10;
+
+    boxesEl.append(div);
+  }  
+}
+  
+
+createBtnEl.addEventListener('click', createBoxes);
+
+destroyBtnEl.addEventListener('click', () => {
+  controlsEL.value = '';
+  boxesEl.innerHTML = '';
+})
