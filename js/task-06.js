6f5f7655ed25a75450b18259d1aa5f2e9e8b8581
Angelica <lisoveckaya1997@gmail.com>
2023-02-21 19:52:07 +0200

    changed 6 task

--- js/task-06.js
+++ js/task-06.js
@@ -2,9 +2,17 @@ const inputEl = document.querySelector('#validation-input');
 console.log(inputEl)
 const checkInput = (event) => {
     
-    event.currentTarget.value.trim().length == inputEl.dataset.length ?
-    event.currentTarget.classList.add('valid') :
+   if (event.currentTarget.value.trim().length == inputEl.dataset.length) {
+    event.currentTarget.classList.add('valid');
+    event.currentTarget.classList.remove('invalid');
+
+   }
+   else {
     event.currentTarget.classList.add('invalid')
+    event.currentTarget.classList.remove('valid')
+   }
+
+   
     
 };
 
